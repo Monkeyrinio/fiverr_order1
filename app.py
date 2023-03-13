4a05bd7bf16e1af14dd26ec74eb17fd5f6355d49
Monkeyrinio <117769576+Monkeyrinio@users.noreply.github.com>
2023-03-13 15:27:36 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,65 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import random
+import string  # to process standard python strings
+import warnings
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+
+
+warnings.filterwarnings("ignore")
+
+nltk.download('popular', quiet=True)  # for downloading packages
+# Includes the following already.
+# nltk.download('punkt') # first-time use only
+# nltk.download('wordnet') # first-time use only
+
+with open('knowledge.txt', 'r', encoding='utf8', errors='ignore') as fin:
+    raw = fin.read().lower()
+
+sent_tokens = nltk.sent_tokenize(raw)  # converts to list of sentences
+word_tokens = nltk.word_tokenize(raw)  # converts to list of words
+
+lemmer = WordNetLemmatizer()
+
+
+def LemTokens(tokens):
+    return [lemmer.lemmatize(token) for token in tokens]
+
+
+remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
+
+
+def LemNormalize(text):
+    return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
+
+# Generating response
+def response(user_response):
+    robo_response = ''
+    sent_tokens.append(user_response)
+    TfidfVec = TfidfVectorizer(tokenizer=LemNormalize, stop_words='english')
+    tfidf = TfidfVec.fit_transform(sent_tokens)
+    vals = cosine_similarity(tfidf[-1], tfidf)
+    idx = vals.argsort()[0][-2]
+    flat = vals.flatten()
+    flat.sort()
+    req_tfidf = flat[-2]
+    if (req_tfidf == 0):
+        robo_response = robo_response + "I am sorry! I don't understand you"
+        return robo_response
+    else:
+        robo_response = robo_response + sent_tokens[idx]
+        return robo_response
+
+
+flag = True
+
+title = st.text_input('Enter your Question:', '')
+st.write('Answer: ', response(title))
